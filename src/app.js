9ec3e0c984ef3f5160797c9367e67b4765976019
oscarvadillog <oscar.vadillog@gmail.com>
2017-04-02 17:07:24 +0200

    Added MediaRecorder and Snapshot features. MediaStream -> Snapshots

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 var adapter = require('webrtc-adapter');
 var gui = require('nw.gui');
+var fs = require('fs');
+var ffmpeg = require('fluent-ffmpeg');
+var ffprobePath = require('@ffprobe-installer/ffprobe').path;
+var ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+ffmpeg.setFfmpegPath(ffmpegPath);
+ffmpeg.setFfprobePath(ffprobePath);
 
 // Extend application menu for Mac OS
 if (process.platform == "darwin") {
@@ -11,7 +17,49 @@ if (process.platform == "darwin") {
 navigator.webkitGetUserMedia(
   {video: true, audio: true},
   function(stream) {
+    var options = {mimeType: 'video/webm;codecs=vp8'};
+    var mediaRecorder = new MediaRecorder(stream, options);
+
+    mediaRecorder.ondataavailable = function(e) {
+      if(e.data.size > 0) {
+        var fileName = 'Record-'+Date.now();
+        var chunks = [];
+        chunks.push(e.data);
+        var blob = new Blob(chunks, {type: 'video/webm'});
+        var fileReader = new FileReader();
+        fileReader.onload = function() {
+          fs.writeFileSync(fileName + '.webm', Buffer.from(new Uint8Array(this.result)));
+          ffmpeg(fileName + '.webm')
+            .output(fileName + '.avi')
+            .on('end', function() {
+              ffmpeg(fileName + '.avi')
+                .on('end', function() {
+                  fs.unlink(fileName + '.webm', function(err){ });
+                  fs.unlink(fileName + '.avi', function(err){ });
+                })
+                .screenshots({
+                  count: 150, // 5seconds * 30fps = 150
+                  filename: fileName,
+                  folder: 'snapshots',
+                  size: '650x480'
+                });
+            })
+            .run()
+        };
+        fileReader.readAsArrayBuffer(blob);
+      }
+    }
+    mediaRecorder.start();
+    var timer = 0;
+    var myInterval = setInterval(function(){
+        timer++;
+        if(timer >= 3) clearInterval(myInterval);
+        mediaRecorder.stop();
+        mediaRecorder.start();
+    }, 5000);
+
     document.getElementById('mirror').src = URL.createObjectURL(stream);
+
   },
   function() {
     alert('Could not connect stream');
